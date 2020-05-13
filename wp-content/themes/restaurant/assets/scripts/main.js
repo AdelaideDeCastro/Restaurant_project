0c12a3def58ab250b4d2f41a07495ab0253ce7b8
AdelaideDeCastro <31382287+AdelaideDeCastro@users.noreply.github.com>
2020-05-13 14:28:42 +0200

    Add files via upload

---
+++ wp-content/themes/restaurant/assets/scripts/main.js
@@ -0,0 +1,13 @@
+// Only use this file to import Javascript files
+// The order of the scripts is important!
+
+// Please remove scripts from the list which are not being used
+
+// via node_modules super easy to update packages
+
+//@prepros-append ../../node_modules/jquery/dist/jquery.js
+//@prepros-append ../../node_modules/slick-carousel/slick/slick.js
+//@prepros-append ../../node_modules/lightbox2/dist/js/lightbox.js
+//@prepros-append ../../node_modules/bootstrap/js/dist/util.js
+//@prepros-append ../../node_modules/bootstrap/js/dist/tab.js
+//@prepros-append custom/main.js
