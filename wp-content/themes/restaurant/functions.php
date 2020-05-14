87803c5a9ffb8babd098a71f72b1840e2aec0f30
Nathalie <nathaliesmeele@gmail.com>
2020-05-14 14:20:33 +0200

    WP updates Bugfixes loading theme Updated npm

--- wp-content/themes/restaurant/functions.php
+++ wp-content/themes/restaurant/functions.php
@@ -2,10 +2,10 @@
 function restaurant_enqueue_script() {
 
 	// add custom css file	
-	wp_enqueue_style( 'customestyle', get_template_directory_uri(). '/assets/css/main.css' );
+	wp_enqueue_style( 'main-css', get_template_directory_uri(). '/assets/css/main.css' );
 
 	// add custom js file
-	wp_enqueue_scripts( 'customscripts', get_template_directory_uri(). '/assets/js/main.min.js', true );
+	wp_enqueue_script( 'main-js', get_template_directory_uri(). '/assets/js/main.min.js', array(), null, true );
 
 }
 
