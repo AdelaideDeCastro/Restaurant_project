efb47c07df7d77ef9d9e9a919fdb0a3f464a024e
Nathalie <nathaliesmeele@gmail.com>
2020-05-14 14:32:12 +0200

    Minor bugfixes on header

--- wp-content/themes/restaurant/header.php
+++ wp-content/themes/restaurant/header.php
@@ -1,16 +1,23 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
 <head>
-    <!-- <meta charset="UTF-8">
-    <meta name="robots" content="noindex, nofollow">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge"> -->
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <?php wp_head(); ?>
 
 </head>
 
-<body>
+<body <?php body_class(); ?>>
+
+<?php
+/*
+ * This is new since WP 5.2
+ */
+wp_body_open();
+?>
+
 <header class="bg-secondary position-sticky">
     <div class="container">
         <div class="d-flex py-2">
