bef762622e5dc7da78e07824e7694c8b674c0fe3
Nathalie <nathaliesmeele@gmail.com>
2020-06-08 15:06:29 +0200

    content archive

--- wp-content/themes/adelaide_restaurant/partials/content-archive.php
+++ wp-content/themes/adelaide_restaurant/partials/content-archive.php
@@ -1,79 +1,105 @@
-<?php 
-$paged = (get_query_var('page')) ? get_query_var('page') : 1;
+<?php
+
+$custom_query_args = ( get_query_var( 'page' ) ) ? get_query_var( 'page' ) : 1;
 
 $args = [
-    'post_type'         => 'last_news', 
+    'post_type'         => 'last_news',
     'posts_per_page'    => 9,
-    'paged'             => $paged,
+    'paged'             => $custom_query_args,
 ];
 
-var_dump( $paged ); // i'm not on this page, see that? for some reason i'm on another page
-
-$wp_query = new WP_Query( $args );
-
-// allright i'm sure i'm on the right page/template
+$news = new WP_Query( $args );
 
 $counter = 1;
-?>
 
-<div class="menu-wrapper w-100 my-5">
+if ( $news->have_posts() ) : ?>
 
-    <div class=" d-flex flex-wrap">
+    <div class="menu-wrapper w-100 my-5">
 
-        <?php
-        while ($wp_query->have_posts()) : $wp_query->the_post(); 
+        <div class=" d-flex flex-wrap">
 
-            $excerpt = get_the_excerpt( $wp_query->ID );
+            <?php
+            while ( $news->have_posts() ) :
+        
+                $news->the_post(); 
 
-            $short_excerpt = preg_match('/^([^.!?\s]*[\.!?\s]+){0,40}/', strip_tags( $excerpt ), $abstract );
+                $excerpt = get_the_excerpt( $news->ID );
 
-            $short_excerpt = $abstract[0] . '[...]';
-        ?>
+                $short_excerpt = preg_match('/^([^.!?\s]*[\.!?\s]+){0,40}/', strip_tags( $excerpt ), $abstract );
 
-            <article class="menu-card nieuws-card<?php if ( $counter >= 4 ){ echo' mt-5'; } else { echo''; } ?>">
-                <a href="<?= esc_url( get_permalink( $wp_query->ID ) ); ?>">
+                $short_excerpt = $abstract[0] . '[...]';
+                ?>
 
-                    <?php
-                    if ( $counter % 2 ) { ?>
-                
-                        <div class="menu-slick-img bg-img bg-img-menu-1" style="background-image: url(' <?= the_post_thumbnail_url( 'full' ); ?> ');"></div>
-                        <div class="slick-content text-center">
-                            <p style="margin-bottom: 6px;"><?= the_date( 'd/m/Y' ); ?></p>
-                            <h2 class="text-primary mb-3"><?= the_title(); ?></h2>
-                            <?= $short_excerpt; ?>
-                        </div>
+                <article class="menu-card nieuws-card<?php if ( $counter >= 4 ){ echo' mt-5'; } else { echo''; } ?>">
+                    <a href="<?= esc_url( get_permalink( $news->ID ) ); ?>">
 
-                    <?php
-                    } else { ?>
+                        <?php
+                        if ( $counter % 2 ) { ?>
+                    
+                            <div class="menu-slick-img bg-img bg-img-menu-1" style="background-image: url(' <?= the_post_thumbnail_url( 'full' ); ?> ');"></div>
+                            <div class="slick-content text-center">
+                                <p style="margin-bottom: 6px;"><?= the_date( 'd/m/Y' ); ?></p>
+                                <h2 class="text-primary mb-3"><?= the_title(); ?></h2>
+                                <?= $short_excerpt; ?>
+                            </div>
 
-                        <div class="slick-content text-center">
-                            <p style="margin-bottom: 6px;"><?= the_date( 'd/m/Y' ); ?></p>
-                            <h2 class="text-primary mb-3"><?= the_title(); ?></h2>
-                            <?= $short_excerpt; ?>
-                        </div>
-                        <div class="menu-slick-img bg-img bg-img-menu-1" style="background-image: url(' <?= the_post_thumbnail_url( 'full' ); ?> ');"></div>
+                        <?php
+                        } else { ?>
 
-                    <?php
-                    }
-                    ?>
-                </a>
-            </article>
+                            <div class="slick-content text-center">
+                                <p style="margin-bottom: 6px;"><?= the_date( 'd/m/Y' ); ?></p>
+                                <h2 class="text-primary mb-3"><?= the_title(); ?></h2>
+                                <?= $short_excerpt; ?>
+                            </div>
+                            <div class="menu-slick-img bg-img bg-img-menu-1" style="background-image: url(' <?= the_post_thumbnail_url( 'full' ); ?> ');"></div>
 
-        <?php
+                        <?php
+                        }
+                        ?>
+                    </a>
+                </article>
+        
+            <?php
             $counter++;
 
-  $total_pages = $wp_query->max_num_pages;
-    $big = 999999999;
-    echo paginate_links( array(
-            'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-            'format' => '/page/%#%',
-            'current' => $paged,
-            'total' => $total_pages,
-            'prev_text' => 'Vorige',
-            'next_text' => 'Volgende'
-        ));
+            endwhile;
+            ?>  
 
-        endwhile; ?>
+        </div>
 
     </div>
-</div>
+
+    <?php
+    if ( $news->max_num_pages > 1 ) { ?>
+
+        <div class="pagination text-center">
+
+            <?php
+            $big = 999999999;
+
+            echo paginate_links( array(
+                'base'         => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+                'format'       => '?paged=%#%',
+                'current'      => max( 1, get_query_var( 'paged' ) ),
+                'total'        => $news->max_num_pages,
+                'show_all'     => false,
+                'type'         => 'plain',
+                'end_size'     => 2,
+                'mid_size'     => 1,
+                'prev_next'    => true,
+                'prev_text'    => sprintf( '<i class="fas fa-angle-left"></i> %1$s', __( '', 'text-domain' ) ),
+                'next_text'    => sprintf( '%1$s <i class="fas fa-angle-right"></i>', __( '', 'text-domain' ) ),
+                'add_args'     => false,
+                'add_fragment' => '',
+            ) );
+           ?>
+
+        </div>
+
+    <?php
+    }
+                    
+endif;
+
+wp_reset_postdata();
+?>
